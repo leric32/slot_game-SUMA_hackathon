5d4e921f351ab90ba939a41af094378276a6a186
Aleksa Racic <racicaleksa7@gmail.com>
2022-04-09 17:32:17 +0200

    Refactoring

--- RTP_calc/paytable.py
+++ RTP_calc/paytable.py
@@ -1,69 +1,66 @@
 test_matrix=[
-    [2,2,3,4],   #<------na
+    [3,2,3,4],   #<------na
     [2,2,4,2],
     [2,4,2,3],
-    [4,6,3,2]
+    [2,6,3,2]
 ]
 
-def check_row(matrix, row):
-    counter = 1
-    max_count = 1
+def check_4row(matrix, row):
     first =  matrix[0][row]
-    val = first
     for i in range(1,len(matrix)):
         if first != matrix[i][row]:
-            if counter > max_count:
-                max_count = counter 
-                counter = 1
-                val = first
-            first = matrix[i][row]
-        else:
-            counter += 1
-    if counter > max_count: 
-        max_count = counter
-    return {'val': val, 'count': max_count}
+            return None
+    return first
 
-def check_major_d(matrix):
-    counter = 1
-    max_count = 1
+def check_3row(matrix, row):
+    if matrix[1][row] == matrix[2][row]:
+        if matrix[0][row] == matrix[1][row]:
+            return matrix[0][row]
+        elif matrix[2][row] == matrix[3][row]:
+            return matrix[3][row]
+    return None
+
+def check_major_4d(matrix):
     first = matrix[0][0]
-    val = first
     for i in range(1,len(matrix)):
         if first != matrix[i][i]:
-            if counter > max_count:
-                max_count = counter 
-                counter = 1
-                
-                val = first
-            first = matrix[i][i]
-        else:
-            counter += 1
-    if counter > max_count: 
-        max_count = counter
-    return {'val': val, 'count': max_count}
+            return None
+    return first
 
-def check_minor_d(matrix):
-    counter = 1
-    max_count = 1
+def check_major_3d(matrix):
+    if matrix[1][1] == matrix[2][2]:
+        if matrix[0][0] == matrix[1][1]:
+            return matrix[0][0]
+        elif matrix[2][2] == matrix[3][3]:
+            return matrix[3][3]
+    return None
+
+
+def check_minor_4d(matrix):
     max_ind = len(matrix) - 1
     first = matrix[0][max_ind]
-    val = first
-    for i in range(1,max_ind-1):
+    for i in range(1,max_ind):
         if first != matrix[i][max_ind-1]:
-            if counter > max_count:
-                max_count = counter 
-                counter = 1
-                
-                val = first
-            first = matrix[i][max_ind-1]
-        else:
-            counter += 1
-    if counter > max_count: 
-        max_count = counter
-    return {'val': val, 'count': max_count}
+            return None
+    return first
+
+def check_minor_3d(matrix):
+    max_ind = len(matrix) - 1
+    if matrix[1][max_ind - 1] == matrix[2][max_ind - 2]:
+        if matrix[0][max_ind - 0] == matrix[1][max_ind - 1]:
+            return matrix[0][max_ind - 0]
+        elif matrix[2][max_ind - 2] == matrix[3][max_ind - 3]:
+            return matrix[3][max_ind - 3]
+    return None
 
 if __name__ == "__main__":
-    print(check_major_d(test_matrix))
-    print(check_minor_d(test_matrix))
+    print(check_major_3d(test_matrix))
+    print(check_major_4d(test_matrix))
+    print(check_minor_3d(test_matrix))
+    print(check_minor_4d(test_matrix))
+    print("4row")
     for i in range(len(test_matrix)):
-        print(check_row(test_matrix, i))
+        print(check_4row(test_matrix, i))
+    print("3row")
+    for i in range(len(test_matrix)):
+        print(check_3row(test_matrix, i))
